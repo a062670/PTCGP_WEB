d6a6b7e67bc58bf07093b2dffcdec8358118c3eb
gagu-home <gagu@insowe.com>
2026-02-06 11:06:20 +0800

    調整node版本及部屬腳本

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
+cd /var/projects/PTCGP_WEB
+
+git reset --hard HEAD
+git pull
+
 cd /var/projects/PTCGP_WEB/client
 npm ci
 npm run build
 
 cp -r /var/projects/PTCGP_WEB/client/dist/spa/* /var/projects/PTCGP_WEB/server/public
+
+cd /var/projects/PTCGP_WEB/server
+npm ci
